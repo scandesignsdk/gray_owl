5ddf58dc708644f38213d5b9cb5cdb4cd1112cb5
Martin Aarhof <martin.aarhof@gmail.com>
2017-07-05 12:37:01 +0200

    Updated the import script

--- src/Import/Import.php
+++ src/Import/Import.php
@@ -1,25 +1,8 @@
 <?php
 namespace SDM\Import;
 
-class Import
+class Import implements ImportInterface
 {
-
-    /**
-     * All products imported
-     *
-     * @var ProductInterface[]
-     */
-    private $products = [];
-
-    /**
-     * @var string
-     */
-    private $filePath;
-
-    /**
-     * @var string
-     */
-    private $delimiter;
 
     /**
      * Importer
@@ -27,42 +10,8 @@ class Import
      * @param string $filePath The path to the csv file
      * @param string $delimiter CSV delimter
      */
-    public function __construct(string $filePath, $delimiter = ',')
+    public function __construct(string $filePath, string $delimiter = ',')
     {
-        $this->filePath = $filePath;
-        $this->delimiter = $delimiter;
-    }
-
-    /**
-     * Parse the csv file
-     *
-     * @return void
-     */
-    public function parse(): void
-    {
-
-    }
-
-    /**
-     * Get products imported
-     *
-     * @return ProductInterface[]
-     */
-    public function getProducts(): array
-    {
-        return $this->products;
-    }
-
-    /**
-     * Add product to the importer
-     *
-     * @param ProductInterface $product
-     * @return Import
-     */
-    public function addProduct(ProductInterface $product): Import
-    {
-        $this->products[] = $product;
-        return $this;
     }
 
 }
---
+++ src/Import/ImportInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SDM\Import;
+
+interface ImportInterface
+{
+
+    /**
+     * Parse the csv file
+     *
+     * @return void
+     */
+    public function parse(): void;
+
+    /**
+     * Get products imported
+     *
+     * @return ProductInterface[]
+     */
+    public function getProducts(): array;
+
+    /**
+     * Add product to the importer
+     *
+     * @param ProductInterface $product
+     * @return Import
+     */
+    public function addProduct(ProductInterface $product): Import;
+
+}
